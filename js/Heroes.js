ba0e6b1ea47a5ca02972a2cc2000d9642017765e
kristian.ignatov <christianignatov@gmail.com>
2017-09-05 21:14:01 +0300

    0.1.0

--- js/Heroes.js
+++ js/Heroes.js
@@ -5,25 +5,38 @@ export default class Heroes {
 	constructor($container) {
 		this.$container = $container;
 		const $menu = this.$container.find('.hero-gallery_menu');
+
 		heroes.forEach((hero, index) => {
 			const $heroBtn = $(`<div class='hero-gallery_btn' data-id='${index}'></div>`);
 			$heroBtn.css('background-image', `url("./assets/images/gallery/${hero.image}_icon.png")`);
 			$menu.append($heroBtn);
 			$heroBtn.on('click', this.onHeroClicked.bind(this, index));
+
+			if (index === 0) {
+				$heroBtn.trigger('click');
+			}
 		});
 	}
 
 	onHeroClicked(index, event) {
 		const hero = heroes[index];
+		const $button = $(event.currentTarget);
+
+		if ($button.hasClass('active')) return;
 
 		this.$container.find('.hero-gallery_btn.active').removeClass('active');
-		$(event.currentTarget).addClass('active');
+		$button.addClass('active');
 
-		this.$container.find('.hero-gallery_name').html(hero.name);
-		this.$container.find('.hero-gallery_real-name').html(hero.realName);
-		this.$container.find('.hero-gallery_age').html(hero.age);
-		this.$container.find('.hero-gallery_origin').html(hero.origin);
-		this.$container.find('.hero-gallery_description').html(hero.description);
+		setTimeout(() => {
+			this.$container.find('.hero-gallery_content').removeClass('buzz-in');
+		}, 400);
+
+		this.$container.find('.hero-gallery_content').addClass('buzz-in');
+		this.$container.find('.hero-gallery_name').text(hero.name);
+		this.$container.find('.hero-gallery_real-name').text(hero.realName);
+		this.$container.find('.hero-gallery_age').text(hero.age);
+		this.$container.find('.hero-gallery_origin').text(hero.origin);
+		this.$container.find('.hero-gallery_description').text(hero.description);
 		this.$container.find('.hero-gallery_image').attr('src', `./assets/images/gallery/gallery_image_${hero.image}.png`);
 	}
 }
